9ec6df4d68b733f8706eb8fe9126ea6517eb0d35
Christian <zarski@gmail.com>
2017-10-25 15:21:34 -0400

    Fix duplicate history event issue

--- README.md
+++ README.md
@@ -7,14 +7,9 @@
 - `cd qbo-audit-log-to-csv`
 - `npm install --global yo gulp-cli bower`
 - `npm install -g generator-chrome-extension`
-- Goto `chrome://extensions`
+- Within Chrome go to `chrome://extensions`
 - Install the extension in developer mode.
   - The folder you want to navigate to is `qbo-audit-log-to-csv\app`
   - The `dist` folder is for production distribution
 - `gulp watch`
 - Almost all of the code is in `app\scripts.babel\contentscript.js`
-- **Important**: Sometime the livereload doesn't work, which then you need to manual refresh from the `chrome://extensions` page.
-
-### Todo
-- Have in only run in qbo.intuit.com/audit/log
-
--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -8,8 +8,8 @@ chrome.runtime.onInstalled.addListener(details => {
 
 chrome.webNavigation.onHistoryStateUpdated.addListener( (details) => {
   console.log('QboAudit Page uses History API and we heard a pushSate/replaceState.')
-  if(typeof chrome._URL === 'undefined' || chrome._URL !== details.url){
-    chrome._URL = details.url
+  if(typeof chrome._LAST_RUN === 'undefined' || notRunWithinTheLastSecond(details.timeStamp)){
+    chrome._LAST_RUN = details.timeStamp
     chrome.tabs.getSelected(null, function (tab) { 
       if(tab.url.match(/.*\/app\/auditlog$/)){
         chrome.tabs.sendRequest(tab.id, 'runQboAuditLog')
@@ -17,3 +17,12 @@ chrome.webNavigation.onHistoryStateUpdated.addListener( (details) => {
     })
   }
 })
+
+const notRunWithinTheLastSecond = (dt) => {
+  const diff = dt - chrome._LAST_RUN
+  if (diff < 1000){
+    return false
+  } else {
+    return true
+  }
+}
